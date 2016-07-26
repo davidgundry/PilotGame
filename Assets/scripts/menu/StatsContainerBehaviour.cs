61fad88f17245dfed160d3b929180a549805b018
David Gundry <davidgundry@gmail.com>
2016-07-26 17:01:08 +0100

    End stats

--- Assets/scripts/menu/StatsContainerBehaviour.cs
+++ Assets/scripts/menu/StatsContainerBehaviour.cs
@@ -14,8 +14,8 @@ namespace menu
         public void Create(PlayerLevelData playerLevelData)
         {
             TimeSpan t = TimeSpan.FromSeconds(playerLevelData.Time);
-            stats1.text = string.Format("{0:D2}:{1:D2}\n{2}\n{3}", t.Minutes, t.Seconds, playerLevelData.Crashes,playerLevelData.Distance);
-            stats2.text = playerLevelData.Time + "\n" + playerLevelData.Crashes + "\n" + playerLevelData.Distance;
+            stats1.text = string.Format("{0:D2}:{1:D2}\n{2}\n{3}", t.Minutes, t.Seconds, (int) playerLevelData.Distance, playerLevelData.FuelUsed);
+            stats2.text = string.Format("{0}\n{1}\n{2}", playerLevelData.Crashes, playerLevelData.Coins, playerLevelData.Pickups);
         }
     }
 }
