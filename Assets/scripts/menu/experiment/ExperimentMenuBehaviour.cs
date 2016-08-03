65f6e04b896f4202b8133cb0335eff144ee4fe8d
David Gundry <davidgundry@gmail.com>
2016-08-03 15:12:11 +0100

    Created experimental scene and basic menu items

---
+++ Assets/scripts/menu/experiment/ExperimentMenuBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace menu.experiment
+{
+    public class ExperimentMenuBehaviour : MonoBehaviour
+    {
+
+        public void StartExperiment()
+        {
+
+        }
+
+        public void SkipToGame()
+        {
+            Application.LoadLevel("main-menu");
+        }
+    }
+}
